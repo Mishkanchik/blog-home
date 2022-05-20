b8a7e39ddd4f775de1b589f7ecb7797f7df68d8b
mishkanchik <csprince666@gmail.com>
2022-05-20 17:57:24 +0300

    commit last

--- script.js
+++ script.js
@@ -20,3 +20,16 @@ function clickBack(){
 }
 
 registerBlock.addEventListener("click",clickBack)
+
+const mobileMenu = document.querySelector(".nav-mobile-menu");
+const mainMenu = document.querySelector(".navigation");
+
+mobileMenu.addEventListener("click", function () {
+    mobileMenu.classList.toggle("active-menu");
+    if (mobileMenu.classList.contains("active-menu")) {
+      mainMenu.classList.add("active-menu");
+    } else {
+      mainMenu.classList.remove("active-menu");
+    }
+  });
+  
